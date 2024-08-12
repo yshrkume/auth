b8aaa06b9dd5f76906e386ecffa4d25c20b482e4
yshrkume <yshrkume@gmail.com>
2024-08-12 22:39:23 +0900

    fix config

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-import secrets
+import os
+import datetime
 
 
 class Config:
-    SECRET_KEY = secrets.token_hex(32)
-    JWT_SECRET_KEY = secrets.token_hex(32)
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your_jwt_secret_key")
+    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=365)
     SQLALCHEMY_DATABASE_URI = "sqlite:///auth.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
